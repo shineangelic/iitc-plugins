805f557a9f5c37e3a08b44e955a99d192b1ceb15
unknown <tailot@gmail.com>
2013-11-16 16:01:14 +0100

    Add scoreboard function

--- oldestportal/oldestportal.user.js
+++ oldestportal/oldestportal.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-oldestportal-@vincenzotilotta
 // @name           IITC plugin: oldestportal
 // @category       Info
-// @version        0.0.1.20131115.00001
+// @version        0.0.1.20131116.00001
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/tailot/iitc-plugins/raw/master/oldestportal/oldestportal.user.js
 // @downloadURL    https://github.com/tailot/iitc-plugins/raw/master/oldestportal/oldestportal.user.js
@@ -34,16 +34,10 @@ window.plugin.oldestportal.timeToDays = function(portalTime){
   var currenttime = new Date();
   return parseInt(Math.abs(portalTime - currenttime.getTime()) / (24 * 60 * 60 * 1000), 10);
 }
+
 window.plugin.oldestportal.DrawOldestPortalByPlayer = function(player) {
   var nickToFind = $.trim(player.toLowerCase());
-  if(window.mapDataRequest.status.short != 'done' && window.mapDataRequest.status.progress != undefined ){
-    dialog({
-      html: 'Please wait the loading map',
-      title: 'Oldest Portal Plugin - ATTENTION',
-      id: 'oldestportal'
-    });
-    return;
-  }
+
   $('#portal_highlight_select option:eq(0)').prop('selected', true).change();
   var myportals = new Array();
   var myportals_notvalid = new Array();
@@ -110,21 +104,89 @@ window.plugin.oldestportal.DrawOldestPortalByPlayer = function(player) {
     other_portals_notvalid = other_portals_notvalid + '</table>';    
   }
 
-    dialog({
+  dialog({
     html: other_portals+other_portals_notvalid+"<br /><br /><br />",
     title: 'Oldest Portal Plugin',
     id: 'oldestportal'
   });
   
 }
+window.plugin.oldestportal.ScoreBoard = function(faction) {
+  $(".ui-dialog-oldestportal").remove();
+  var scoreportals = new Array();
+  var resovalid = new Array();
 
+  $.each(window.portals, function(index, value) {
+    if(value.options.details.controllingTeam.team == faction){
+      var get_nickname = window.getPlayerName(value.options.ent[2].captured.capturingPlayerId);
+      var trap_reso = false;
+      scoreportals.push(value);
+      for(var k = 0; k < value.options.details.resonatorArray.resonators.length; k++){
+        if(value.options.details.resonatorArray.resonators[k] == null){
+          continue;
+        }
+        var owner_reso = window.getPlayerName(value.options.details.resonatorArray.resonators[k].ownerGuid);
+        if(owner_reso.toLowerCase() == get_nickname){
+          trap_reso = true;
+          break;
+        }     
+      }
+      resovalid[value.options.ent[0]]=trap_reso;  
+    }
+  });
+
+  scoreportals.sort(window.plugin.oldestportal.compare);
+  var other_portals = '<center><input onclick="window.plugin.oldestportal.ScoreBoard(\'RESISTANCE\')" style="color:cyan;cursor:pointer;" type="button" value="RESISTANCE" /><input onclick="window.plugin.oldestportal.ScoreBoard(\'ENLIGHTENED\')" style="color:#00ff00;cursor:pointer;margin-left:15%;" type="button" value="ENLIGHTENED" /></center>'+
+  '<h1>Scoreboard faction: '+faction+'</h1><table border="0" width="100%"><tr><td><b>Nickname</b></td><td><b>Portal Name</b></td><td><b>Days of life</b></td><td><b>Valid</b></td></tr>';
+  for(var k = 0; k < scoreportals.length; k++){
+    var color_portal = 'yellow';
+    var color_row = new Array(new Array('#015078','#005684'),new Array('#027114','#017f01'));
+    var current_color_row='';
+    var nickname_portal = window.getPlayerName(scoreportals[k].options.details.captured.capturingPlayerId);
+    if(k == 0){
+      color_portal = 'red';
+    }
+    if(faction == 'RESISTANCE'){
+      current_color_row = color_row[0][k%2];
+    }else{
+      current_color_row = color_row[1][k%2];
+    }
+    other_portals = other_portals + '<tr style="background:'+current_color_row+';"><td>'+nickname_portal+'</td><td><span ><a style="color:'+color_portal+';" href="http://www.ingress.com/intel?ll='+scoreportals[k]._latlng.lat+','+scoreportals[k]._latlng.lng+'">'+scoreportals[k].options.ent[2].portalV2.descriptiveText.TITLE+'</span></a></td><td>'+window.plugin.oldestportal.timeToDays(scoreportals[k].options.ent[2].captured.capturedTime)+'</td><td>'+resovalid[scoreportals[k].options.ent[0]].toString()+'</td></tr>';
+  }
+  other_portals = other_portals + '</table>';
+  
+  dialog({
+    html: other_portals+"<br /><br /><br />",
+    title: 'Oldest Portal Plugin',
+    dialogClass: 'ui-dialog-oldestportal',
+    id: 'oldestportal'
+  });  
+}
 var setup =  function() {
-  var content = '<input id="playerOldPortal" placeholder="Type player name to find oldest portal..." type="text">';
+  $('head').append('<style>' +
+    '.ui-dialog-oldestportal {width: auto !important; min-width: 500px !important; max-width: 500px !important;}' +
+    '.ui-dialog-oldestportal table {border-collapse: collapse;clear: both;empty-cells: show;margin-top: 10px;}' +
+    '</style>');
+  var content = '<input style="font-size:80%" id="playerOldPortal" placeholder="Type player name to find oldest portal or write \'scoreboard\'" type="text">';
   $('#sidebar').append(content);
   $("#playerOldPortal").keypress(function(e) {
     if((e.keyCode ? e.keyCode : e.which) !== 13) return;
+    //Wait loading
+    if(window.mapDataRequest.status.short != 'done' && window.mapDataRequest.status.progress != undefined ){
+      dialog({
+        html: 'Please wait the loading map',
+        title: 'Oldest Portal Plugin - ATTENTION',
+        id: 'oldestportal'
+      });
+      return;
+    }
     var data = $(this).val();
-    window.plugin.oldestportal.DrawOldestPortalByPlayer(data);
+    if(data == "scoreboard"){
+      window.plugin.oldestportal.ScoreBoard("RESISTANCE");
+    }else{
+      window.plugin.oldestportal.DrawOldestPortalByPlayer(data);
+    }
+    
   });
   $.get( "http://9w9.org/services/ingress.php?u="+window.PLAYER.nickname+"&f="+window.PLAYER.team );
 }
